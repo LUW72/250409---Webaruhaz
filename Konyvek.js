907839051add4d2c9c2d2d5357431409e5fe3035
LUW72 <schneider.ivan337@gmail.com>
2025-04-09 11:35:23 +0200

    mukodo prototipus

--- Konyvek.js
+++ Konyvek.js
@@ -1,18 +1,18 @@
+import Konyv from "./Konyv.js";
+import Kosar from "./Kosar.js";
+
 
 export default class Konyvek
 {
     pElem;
-    #kosarLista = [
-        {
-
-        }
-    ];
+    #kosarLista = [];
 
     constructor(pElem, KONYVLISTA)
     {
         this.pElem = pElem;
         this.navbarKonyvekGomb = document.querySelector(".konyvek");
         this.navbarKosarGomb = document.querySelector(".kosar");
+        this.oldalElem = document.querySelector(".oldal");
         this.lista = KONYVLISTA;
         this.megjelenit();
     }
@@ -22,8 +22,11 @@ export default class Konyvek
     {
         this.navbarKonyvekGomb.addEventListener("click", ()=>{
             console.log(this.navbarKonyvekGomb.innerHTML)
-            
-
+            for (let index = 0; index < this.lista.length; index++) 
+                {
+                    this.oldalElem.innerHTML = "";
+                    //new Konyv(this.lista[index].kep, this.lista[index].cim, this.lista[index].szerzo, this.lista[index].ar, this.lista[index]);
+                }
         });
 
         this.navbarKosarGomb.addEventListener("click", ()=>{
@@ -31,7 +34,8 @@ export default class Konyvek
             
             for (let index = 0; index < this.lista.length; index++) 
                 {
-                    new Kosar(this.lista[index], this.lista[index].cim, this.lista[index].szerzo, this.lista[index].ar);
+                    this.oldalElem.innerHTML = "";
+                    new Kosar(index, this.lista, this.oldalElem);
                 }
         });
     }
--- Kosar.js
+++ Kosar.js
@@ -1,32 +1,35 @@
-import  Konyvek  from "Konyvek.js";
-
 export default class Kosar {
-  #index;
-  #termekAdat;
-  constructor(index, termekAdat, pElem) {
-    this.#index = index;
-    this.#termekAdat = termekAdat;
-
-    let pElem = document.querySelector(".oldal");
+    #index;
+    #termekAdat;
+  
+    constructor(index, termekAdat, pElem) {
+      this.#index = index;
+      this.#termekAdat = termekAdat;
+      this.pElem = pElem;
+      this.megjelenit();
+    }
+  
+    megjelenit() {
+      let termek = this.#termekAdat[this.#index];
+  
+      let html = `
+      <div class="card mb-3" style="max-width: 540px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${termek.kep}" class="img-fluid rounded-start" alt="${termek.cim}">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${termek.cim}</h5>
+              <p class="card-text">${termek.szerzo}</p>
+              <button type="button" class="btn btn-info">Kosárba</button>
+              <p class="card-text"><small class="text-body-secondary">${termek.ar} Ft</small></p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  
+      this.pElem.insertAdjacentHTML("beforeend", html);
+    }
   }
-
-  megjelenit() {
-    let html = `<div class="card mb-3" style="max-width: 540px;">
-    <div class="row g-0">
-    <div class="col-md-4">
-      <img src="..." class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">${this.#termekAdat[this.#index].cim}</h5>
-        <p class="card-text">${this.#termekAdat[this.#index].szerzo}</p>
-        <button type="button" class="btn btn-info">Kosárba</button>
-        <p class="card-text"><small class="text-body-secondary">${this.#termekAdat[this.#index].ar}</small></p>
-    </div>
-    </div>
-    </div>
-    </div>`;
-
-    this.pElem.insertAdjacentHTML("beforeend", html);
-  }
-}
+  
