b4f9f1c843851c456c97ae0704930464346b64d5
LUW72 <schneider.ivan337@gmail.com>
2025-04-09 10:50:11 +0200

    minta lista es kep hozzaadasa

---
+++ Konyvek.js
@@ -0,0 +1,23 @@
+export default class Konyvek
+{
+    pElem;
+    konyvLista = [
+        {
+            kep: kepek/dune.jpg,
+            cim: "Dune",
+            szerzo: "Frank Herbert",
+            ar: 5400,
+        },
+        {
+            kep: kepek/dune.jpg,
+            cim: "Második könyv",
+            szerzo: "Szerző neve 2",
+            ar: 4400,
+        },        
+    ];
+    kosarLista = [
+        {
+
+        }
+    ];
+}
